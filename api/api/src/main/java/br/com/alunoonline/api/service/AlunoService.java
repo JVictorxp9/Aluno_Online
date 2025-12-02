7a36f45cb0d577375da8ca8e7f9cf9b4d5f14747
João Victor <129428389+JVictorxp9@users.noreply.github.com>
2025-12-02 06:22:58 -0300

    Update AlunoService.java

--- api/api/src/main/java/br/com/alunoonline/api/service/AlunoService.java
+++ api/api/src/main/java/br/com/alunoonline/api/service/AlunoService.java
@@ -3,27 +3,60 @@ package br.com.alunoonline.api.service;
 import br.com.alunoonline.api.model.Aluno;
 import br.com.alunoonline.api.repository.AlunoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
 
-@Service
+@Service // Indica que a classe abaixo é um componente de serviço do Spring, responsável pela regra de negócio.
 public class AlunoService {
 
-    @Autowired
-    private AlunoRepository alunoRepository;
+    @Autowired // Injeta automaticamente o repositório gerenciado pelo Spring
+    AlunoRepository alunoRepository;
 
-    public void criarAluno(Aluno aluno){
+    // Salva um novo aluno no banco de dados.
+    public void criarAluno(Aluno aluno) {
         alunoRepository.save(aluno);
     }
 
-    public List<Aluno> buscarTodosAlunos(){
+    // Busca todos os aluno no banco de dados.
+    public List<Aluno> buscarTodosAlunos() {
         return alunoRepository.findAll();
     }
 
-    public Optional<Aluno> buscarAlunoPorId(Long id){
-        alunoRepository.findById(id);
-        return Optional.empty();
+    // Busca um aluno no banco de dados.
+    public Optional<Aluno> buscarAlunoPorId(long id) {
+        return alunoRepository.findById(id);
+    }
+
+    public void deletarAlunoPorId(long id) {
+        alunoRepository.deleteById(id);
+    }
+
+    public void atualizarAlunoPorId(Long id, Aluno alunoAtualizado) {
+        // PRIMEIRO PASSO: VER SE O ALUNO EXISTE NO BD
+        Optional<Aluno> alunoDoBancoDeDados = buscarAlunoPorId(id);
+
+        // E SE NÃO EXISTIR O ALUNO COM ESSE ID?
+        if (alunoDoBancoDeDados.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Aluno não encontrado no banco de dados!");
+        }
+
+        // SE CHEGOU AQUI SIGNIFICA QUE EXISTE ALUNO COM ESSE ID!
+        // VOU ARMAZENA-LO EM UMA VARIVEL PARA DEPOIS EDITA-LO
+        Aluno alunoParaEditar = alunoDoBancoDeDados.get();
+
+        // COM ESSE ALUNO PARA SER EDITADO ACIMA, FAÇO
+        // OS SETS NECESSARIOS PARA ATUALIZAR OS ATRIBUTOS DELE
+
+        alunoParaEditar.setNomeCompleto(alunoAtualizado.getNomeCompleto());
+        alunoParaEditar.setEmail(alunoAtualizado.getEmail());
+        alunoParaEditar.setCpf(alunoAtualizado.getCpf());
+
+        // AGORA VOU LEVAR ESSE ALUNO PARA EDITAR PARA O BD
+        alunoRepository.save(alunoParaEditar);
     }
 }
+
